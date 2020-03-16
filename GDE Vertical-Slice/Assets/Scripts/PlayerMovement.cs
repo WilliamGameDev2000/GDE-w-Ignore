75f465b284e482361160ce64fea1dde18b57eff1
will <Hamish2.Lawrence@live.uwe.ac.uk>
2020-03-16 15:49:05 +0000

    Completed Movement?
    (Still needs testing with a mic but I think it will work based on the player moving when the mic input was removed and the mic input should be 0 when the mic isn't being used)

--- GDE Vertical-Slice/Assets/Scripts/PlayerMovement.cs
+++ GDE Vertical-Slice/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    CharacterController character;
+
+    static Hertz hertz;
+
+    public float speed = hertz.getLastVolume();
+
+    public Vector3 movement;
+
     void Start()
     {
-        
+        character = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement = new Vector3(1.0f, 0.0f, 1.0f);
+        movement *= speed;
+
+        character.Move(movement * Time.deltaTime);
     }
 }
