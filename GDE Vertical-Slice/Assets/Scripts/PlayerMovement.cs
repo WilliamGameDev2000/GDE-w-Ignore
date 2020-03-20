e0540e4db2ed48af61c191815e00480a77f147fb
William <Hamish2.Lawrence@live.uwe.ac.uk>
2020-03-20 22:34:42 +0000

    sliders

--- GDE Vertical-Slice/Assets/Scripts/PlayerMovement.cs
+++ GDE Vertical-Slice/Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,10 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = hertz.getLastVolume();
 
+    public float angle;
+
     public Vector3 movement;
+
 
     void Start()
     {
@@ -23,6 +26,19 @@ public class PlayerMovement : MonoBehaviour
         movement = new Vector3(1.0f, 0.0f, 1.0f);
         movement *= speed;
 
+        
         character.Move(movement * Time.deltaTime);
+        character.transform.Rotate(0, angle, 0);
+        
+    }
+
+    public void AdjustSlider(float newSpeed)
+    {
+        speed = 20 * newSpeed;
+    }
+
+    public void changeAngle(float newAngle)
+    {
+        angle = newAngle;
     }
 }
