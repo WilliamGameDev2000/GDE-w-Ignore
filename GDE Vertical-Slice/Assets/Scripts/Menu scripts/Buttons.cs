9dcc497f4efc534e86a332a71884e638184281bd
Charlie <charlie6.evans@live.uwe.ac.uk>
2020-04-02 19:53:02 +0100

    Added simple menu state part 2

---
+++ GDE Vertical-Slice/Assets/Scripts/Menu scripts/Buttons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Buttons : MonoBehaviour
+{
+    public Button startButton;
+    public Button exitButton;
+
+    public string gameScene;
+
+    public void Awake()
+    {
+        startButton.onClick.AddListener(playGame);
+        exitButton.onClick.AddListener(exitGame);
+    }
+
+    public void playGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void exitGame()
+    {
+        // doesn't work in editor
+        Application.Quit();
+        Debug.Log("EXIT");
+    }
+}
