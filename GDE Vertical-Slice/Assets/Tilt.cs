c892e32f561e429bf1018e1531e2c15fae511301
Charlie <charlie6.evans@live.uwe.ac.uk>
2020-04-21 22:39:11 +0100

    Implementing A Simple puzzle

---
+++ GDE Vertical-Slice/Assets/Tilt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilt : MonoBehaviour
+{
+    public GameObject board;
+
+    public float angleH;
+    public float angleV;
+
+    void Update()
+    {
+        board.transform.localRotation = Quaternion.Euler(-angleH, 0.0f, angleV);
+    }
+
+    public void changeHorizontalAngle(float newAngle)
+    {
+        angleH = newAngle;
+    }
+
+    public void changeVerticalAngle(float newAngle)
+    {
+        angleV = newAngle;
+    }
+}
