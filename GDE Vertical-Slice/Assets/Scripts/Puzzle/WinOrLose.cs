c78ab0c25b31e50e9215fba53ffc956ef0e49588
Vladislav <Vladislav2.Trakiyski@live.uwe.ac.uk>
2020-04-22 12:14:25 +0100

    Please Work

---
+++ GDE Vertical-Slice/Assets/Scripts/Puzzle/WinOrLose.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinOrLose : MonoBehaviour
+{
+    public Collider Ball;
+    public GameObject Sliders;
+    public GameObject Cam1;
+    public GameObject Cam2;
+
+    public Tilt PuzzleController;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == Ball)
+        {
+            Sliders.SetActive(false);
+            Cam1.SetActive(false);
+            Cam2.SetActive(true);
+            FindObjectOfType<AudioManager>().StopCoroutine("BallRolling");
+            FindObjectOfType<AudioManager>().Play("Win");
+            PuzzleController.rolling = false;
+
+
+        }
+    }
+}
--- GDE Vertical-Slice/Assets/Tilt.cs
+++ GDE Vertical-Slice/Assets/Tilt.cs
@@ -9,18 +9,60 @@ public class Tilt : MonoBehaviour
     public float angleH;
     public float angleV;
 
+    public bool rolling = true;
+
+    private void Start()
+    {
+        //Theme Sound Start on level start
+        FindObjectOfType<AudioManager>().Play("PuzzleTheme");
+    }
+
     void Update()
     {
         board.transform.localRotation = Quaternion.Euler(-angleH, 0.0f, angleV);
+        
     }
 
     public void changeHorizontalAngle(float newAngle)
     {
         angleH = newAngle;
+        //Ball Rolling sound added
+        if (angleH > 0.5f)
+        {
+            if (rolling)
+            {
+                FindObjectOfType<AudioManager>().Play("BallRolling");
+            }
+        }
+        else if (angleV < 0)
+        {
+            if (rolling)
+            {
+                FindObjectOfType<AudioManager>().Play("BallRolling");
+            }
+        }
+
     }
 
     public void changeVerticalAngle(float newAngle)
     {
         angleV = newAngle;
+        //Ball Rolling sound added
+        if (angleV > 0.3f)
+        {
+            if (rolling)
+            {
+                FindObjectOfType<AudioManager>().Play("BallRolling");
+            }
+        }
+        else if (angleV < 0)
+        {
+            if (rolling)
+            {
+                FindObjectOfType<AudioManager>().Play("BallRolling");
+            }
+        }
+        
+
     }
 }
